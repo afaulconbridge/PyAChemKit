30ec066a8f87fd72226a2e5570bcaf98801ba9a3
Adam Faulconbridge <afaulconbridge@googlemail.com>
2011-10-31 19:20:08 +0000

    Added simple stat code

---
+++ achemkit/utils/stat.py
@@ -0,0 +1,20 @@
+
+
+def mean(values):
+    return sum(values) / float(len(values))
+    
+def median(values):
+    return sorted(values)[len(values) // 2]
+    
+def quartile_upper(values):
+    return sorted(values)[len(values) // 4]
+    
+def quartile_lower(values):
+    return sorted(values, reverse=True)[len(values) // 4]
+    
+def standard_deviation(values):
+    raise NotImplementedError
+    
+def standard_error(values):
+    raise NotImplementedError
+    
